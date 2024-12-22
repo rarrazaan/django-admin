d8c2e438680e048b8764db7bd22f6982079fd4fb
Imam Rafiif Arrazaan <58129889+rarrazaan@users.noreply.github.com>
2024-12-22 14:54:40 +0700

    fix: change debug mode

--- powerintadmin/settings.py
+++ powerintadmin/settings.py
@@ -29,7 +29,7 @@ API_TOKEN = os.getenv("API_TOKEN")
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [".vercel.app", "localhost"]
 
